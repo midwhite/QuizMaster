2cccf1fa8d84c98b9c631f6ef28b0e05399f357e
Hiroki Takano <midwhite.development@gmail.com>
2018-07-08 21:04:05 +0900

    enable to use I18n.t on controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
   before_action :set_locale
 
   def check
-    render json: { message: "Success." }
+    render json: { message: t(".success") }
   end
 
   private
@@ -12,5 +12,12 @@ class ApplicationController < ActionController::API
 
   def default_url_options(options = {})
     { locale: I18n.locale }.merge options
+  end
+
+  def t(key, **option)
+    if key[0] === '.'
+      key = controller_name + "_controller." + action_name + key
+    end
+    I18n.t(key, **option)
   end
 end
