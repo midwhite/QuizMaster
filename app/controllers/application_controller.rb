8c939d44983b544209fa376288e0071a90643285
Hiroki Takano <midwhite.development@gmail.com>
2018-07-08 20:53:04 +0900

    set up locale and timezone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  before_action :set_locale
+
+  def check
+    render json: { message: "Success." }
+  end
+
+  private
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options(options = {})
+    { locale: I18n.locale }.merge options
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -19,17 +19,12 @@ Bundler.require(*Rails.groups)
 
 module QuizMaster
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.i18n.default_locale = :en
+    config.time_zone = "UTC"
+    config.active_record.default_timezone = :utc
 
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
   end
 end
